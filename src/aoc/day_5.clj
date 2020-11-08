ef4f9fb7be167dd3980fc1f66a26ef9df3611f76
Brandon Olivier <brandon@brandonolivier.com>
2020-11-08 15:43:02 -0600

    Update day 5

--- src/aoc/day_5.clj
+++ src/aoc/day_5.clj
@@ -1,7 +1,7 @@
 (ns aoc.day-5
   (:require [aoc.intcode :refer [run tokenize]]))
 
-(def input (into [] (tokenize (slurp "resources/day_5.input"))))
+(def input (slurp "resources/day_5.input"))
 
 (defn solution-1 []
   (run input))
